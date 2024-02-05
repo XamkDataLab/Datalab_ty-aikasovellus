a90bb02580409e98640258f00667db83b2a4314e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-02-05 21:12:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,40 +29,55 @@ def insert_hours(job_id, user_id, hours):
     conn.commit()
     conn.close()
 
+if 'selected_job_id' not in st.session_state:
+    st.session_state.selected_job_id = None
+
+def show_job_details(job_id):
+    """Function to set the selected job ID in session state."""
+    st.session_state.selected_job_id = job_id
+    
+#layout
 left_col, right_col = st.columns([2, 1])
 
 with right_col:
     with st.form("login_form", clear_on_submit=True):
-        st.text_input('Käyttäjänimi', max_chars=15)
-        st.text_input('Salasana', type='password', max_chars=15)
+        username_input = st.text_input('Käyttäjänimi', max_chars=15)
+        password_input = st.text_input('Salasana', type='password', max_chars=15)
         submit_button = st.form_submit_button('Kirjaudu')
-        
+
 st.title('Datalab työt')
 jobs_df = get_jobs_data()
-st.dataframe(jobs_df)        
 
+for index, row in jobs_df.iterrows():
+    with st.container():
+        left, right = st.columns([4, 1])
+        left.write(f"{row['JobName']}")
+        select_button = right.button("Valitse", key=row['JobID'])
+        if select_button:
+            show_job_details(row['JobID'])
+
+if st.session_state.selected_job_id is not None:
+    selected_job = jobs_df[jobs_df['JobID'] == st.session_state.selected_job_id].iloc[0]
+    st.write(f"Valittu Työ: {selected_job['JobName']}")
+    st.write(f"Työn Kuvaus: {selected_job['JobDescription']}")
+    
 if submit_button:
     conn = create_connection()
     cursor = conn.cursor()
-    hashed_password = hash_password(password)
-    cursor.execute("SELECT * FROM Users WHERE Username = ? AND Password = ?", username, hashed_password)
+    hashed_password = hash_password(password_input)  # Use the input from the form
+    cursor.execute("SELECT * FROM Users WHERE Username = ? AND Password = ?", username_input, hashed_password)
     user = cursor.fetchone()
     conn.close()
 
     if user:
         st.sidebar.success('Kirjautuminen onnistui')
-        job_list = jobs_df['JobName'].tolist()  
-        selected_job = st.selectbox("Valitse työ", job_list)
-
-        if selected_job:
+        if st.session_state.selected_job_id is not None:
+            selected_job = jobs_df[jobs_df['JobID'] == st.session_state.selected_job_id].iloc[0]
             hours = st.number_input("Tehdyt työtunnit", min_value=0.0, max_value=100.0, step=0.5)
             if st.button("Lisää tunnit"):
-                
-                job_id = jobs_df[jobs_df['JobName'] == selected_job]['JobID'].iloc[0]  
-                user_id = user['Username']  
+                job_id = selected_job['JobID']
+                user_id = user[1]  
                 insert_hours(job_id, user_id, hours)
                 st.success("Tunnit kirjattu onnistuneesti")
-
     else:
         st.sidebar.error('Login failed. Check your username and password.')
-
